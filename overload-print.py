5a33bb0ea78d0345bdc135026de37091c35f45e6
Rewanth Tammana <22347290+rewanthtammana@users.noreply.github.com>
2021-07-11 10:49:19 +0400

    Update overload-print.py

--- overload-print.py
+++ overload-print.py
@@ -7,9 +7,6 @@ except ImportError:
     import builtins as __builtin__
 
 def print(*args, **kwargs):
-    def __repr__(*argss, **kwargss):
-        return __builtin__.repr(*argss, **kwargss)
-
     if type(args[0]) == dict:
         if args[0].get('Password') != None and args[0]['Password'] != "":
             __builtin__.print("{'PassProps.Username': '" + args[0]['PassProps.Username'] + "', 'Password': '*****'}")
